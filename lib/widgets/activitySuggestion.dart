ff711dce5c09756a2266da090d5b053b1af75985
AlbertoJr789 <albertojunior.dev789@gmailc.om>
2024-12-11 16:01:55 -0300

    feat(layout) activity suggestion rework

--- lib/widgets/activitySuggestion.dart
+++ lib/widgets/activitySuggestion.dart
@@ -4,65 +4,74 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class ActivitySuggestion extends StatelessWidget {
-
   final Activity activity;
-  const ActivitySuggestion({super.key,required this.activity});
+  const ActivitySuggestion({super.key, required this.activity});
 
   @override
   Widget build(BuildContext context) {
-    return  Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Row(
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'Há um passageiro precisando de corrida!',
+          style: Theme.of(context).textTheme.bodyLarge,
+        ),
+        const Divider(color: Color.fromRGBO(0, 0, 0, 0.205)),
+        Center(
+          child: Column(
             children: [
-              // Profile Picture
-               ClipRRect(
+              ClipRRect(
                 borderRadius: BorderRadius.circular(25.0),
                 child: CachedNetworkImage(
                     fit: BoxFit.cover,
-                    width: 50,
-                    height: 50,
+                    width: 75,
+                    height: 75,
                     placeholder: (context, url) =>
                         const CircularProgressIndicator(),
-                    errorWidget: (context, url, error) =>
-                        const Icon(Icons.person_off_outlined,color: Colors.black,),
+                    errorWidget: (context, url, error) => const Icon(
+                          Icons.person_off_outlined,
+                          color: Colors.black,
+                        ),
                     imageUrl: activity.passenger!.avatar!),
               ),
-              const SizedBox(width: 10),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    activity.passenger!.name,
-                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                  ),
-                ],
+              Text(
+                activity.passenger!.name,
+                style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
               ),
             ],
           ),
-          const SizedBox(height: 20),
-          Row(
-            children: [
-              const Icon(Icons.flag_circle, size: 20, color: Colors.grey),
-              const SizedBox(width: 8),
-              Text(
+        ),
+        const SizedBox(height: 20),
+        Text('Origem: ',textAlign: TextAlign.left,style: Theme.of(context).textTheme.bodyLarge,),
+        const SizedBox(height: 5),
+        Row(
+          children: [
+            const Icon(Icons.flag, size: 20, color: Colors.grey),
+            const SizedBox(width: 8),
+            Flexible(
+              child: Text(
                 activity.origin.formattedAddress,
-                style: TextStyle(fontSize: 14),
+                style: const TextStyle(fontSize: 14),
               ),
-            ],
-          ),
-          const SizedBox(height: 10),
-          Row(
-            children: [
-              const Icon(Icons.timer_sharp, size: 20, color: Colors.grey),
-              const SizedBox(width: 8),
-              Text(
+            ),
+          ],
+        ),
+        const SizedBox(height: 10),
+        Text('Destino: ',textAlign: TextAlign.left,style: Theme.of(context).textTheme.bodyLarge,),
+        const SizedBox(height: 5),
+        Row(
+          children: [
+            const Icon(Icons.flag_circle, size: 20, color: Colors.grey),
+            const SizedBox(width: 8),
+            Flexible(
+              child: Text(
                 activity.destiny.formattedAddress,
-                style: TextStyle(fontSize: 14),
+                style: const TextStyle(fontSize: 14),
               ),
-            ],
-          ),
-        ],
-      );
+            ),
+          ],
+        ),
+      ],
+    );
   }
 }
